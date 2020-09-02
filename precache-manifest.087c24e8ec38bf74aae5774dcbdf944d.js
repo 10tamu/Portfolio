879fe909520570231843665d8b73554d787f66af
palaktank <101913785@student.swin.edu.au>
2020-09-02 21:49:26 +1000

    Updates

---
+++ precache-manifest.087c24e8ec38bf74aae5774dcbdf944d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb6212f824512b9ec0df06846ae3caa",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "d8935680fc1fbe062b94",
+    "url": "/Portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "535ee47278d9245b99c0",
+    "url": "/Portfolio/static/css/main.2a3348f4.chunk.css"
+  },
+  {
+    "revision": "d8935680fc1fbe062b94",
+    "url": "/Portfolio/static/js/2.f124ef7d.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.f124ef7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "535ee47278d9245b99c0",
+    "url": "/Portfolio/static/js/main.6ea738cc.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  },
+  {
+    "revision": "4f748525e2208911cd80fc16044eaf90",
+    "url": "/Portfolio/static/media/Palak Tank edited.4f748525.png"
+  },
+  {
+    "revision": "3a607952ee3ba88d8aff9cdc3a8902da",
+    "url": "/Portfolio/static/media/WFH.3a607952.jpg"
+  },
+  {
+    "revision": "11828d76bf19cdd96786c6f367aa7aac",
+    "url": "/Portfolio/static/media/face.11828d76.png"
+  },
+  {
+    "revision": "38e8bedb39d8e4338c790e9af34467e4",
+    "url": "/Portfolio/static/media/sample img.38e8bedb.jpg"
+  }
+]);
